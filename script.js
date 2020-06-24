6ecfb799c06b1f6e9a4fff0f595d8dc43f735a90
Jacoblynch99 <jacoblynch99@gmail.com>
2020-06-24 16:58:28 -0500

    finishing touches

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ let arrayOfTodos = [
     "completed": false
   }]
 
+  let elementColorRed = "red"
+  let elementColorGreen = "green"
+
   const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
     .then( (response) => response.json())
@@ -26,20 +29,50 @@ let arrayOfTodos = [
     
     
     let newElement = document.getElementById("todo-list")
+    let fontColor = ""
+
     
     for (let i = 0; i < arrayOfTodos.length; i++) {
-        let newli = document.createElement("LI")
-        let fork = document.createTextNode(arrayOfTodos[i].userId)
-        let fork = document.createTextNode(arrayOfTodos[i].id)
-        let fork = document.createTextNode(arrayOfTodos[i].title)
-        let fork = document.createTextNode(arrayOfTodos[i].completed)
-        newli.appendChild(fork)
-        newElement.appendChild(newli)   
+        
+        
+
+        if (arrayOfTodos[i].completed === true){
+            fontColor = "green"
+            } else {
+            fontColor = "red"    
+            }
+
+        let newLI = document.createElement("LI")
+        let newH1 = document.createElement("P")
+        let newH2 = document.createElement("P")
+        let newH3 = document.createElement("P")
+        let newH4 = document.createElement("P")
+
+        let userId = document.createTextNode(arrayOfTodos[i].userId)
+        newH1.appendChild(userId)
+        
+        let id = document.createTextNode(arrayOfTodos[i].id)
+        newH2.appendChild(id)
+    
+        let title = document.createTextNode(arrayOfTodos[i].title)
+        newH3.appendChild(title)
+
+        let completed = document.createTextNode(arrayOfTodos[i].completed)
+        newH4.appendChild(completed)
+
+        newElement.appendChild(newLI) 
+        newLI.appendChild(newH1)
+        newLI.appendChild(newH2)
+        newLI.appendChild(newH3)
+        newLI.appendChild(newH4)
+
+        newLI.style.color = fontColor
     }
-
-
-
   }
+
+
+
+// document.getElementsByTagName(H4).style.color
 
 console.log(arrayOfTodos[0].title)
 console.log(arrayOfTodos[1].title)
